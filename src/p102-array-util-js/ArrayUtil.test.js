dff27aebfce5eda97d0908d39a42e59fddd821c0
Paulio <pauliorandall@gmail.com>
2026-06-26 00:21:38 +0100

    Test P102-array-util-js

---
+++ src/p102-array-util-js/ArrayUtil.test.js
@@ -0,0 +1,9 @@
+// Tests '@paulio/array-util-js' can be imported and used.
+
+import ArrayUtil from '@paulio/array-util-js'
+
+test("ArrayUtil", () => {
+	const letters = ['A', 'B', 'C']
+	const act = ArrayUtil.last(letters)
+	expect(act).toEqual('C')
+})
